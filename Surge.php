2c38fa1fa13627befcdf07d712b00544bb1dc535
王昭 <877820030@qq.com>
2017-01-26 21:40:50 +0800

    Update Surge.php

--- Surge.php
+++ Surge.php
@@ -1,4 +1,4 @@
-#!MANAGED-CONFIG https://raw.githubusercontent.com/wangzhao1989/Blog/Surge/Surge.php
+#!MANAGED-CONFIG https://raw.githubusercontent.com/wangzhao1989/Blog/Surge/Surge.php interval=60 strict=true
 [General]
 loglevel = notify
 dns-server = system, 114.114.114.114, 223.5.5.5
