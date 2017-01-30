781d8b66b34c0d21971e4e3db6355c1b7fca8bff
王昭 <877820030@qq.com>
2017-01-30 17:41:31 +0800

    Update Surge.php

--- Surge.php
+++ Surge.php
@@ -6,7 +6,7 @@ skip-proxy = 127.0.0.1, 192.168.0.0/16, 10.0.0.0/8, 172.16.0.0/12, 100.64.0.0/10
 ipv6 = true
 port = 8888
 socks-port = 8889
-external-controller-access = apassword@127.0.0.1:8888
+external-controller-access = apassword@127.0.0.1:8888oooo
 allow-wifi-access = true
 
 # For iOS
