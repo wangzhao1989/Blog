e8aa7d601939fb5302f88a6dd3c7cc7cb743bf46
王昭 <877820030@qq.com>
2019-12-12 10:42:44 +0800

    Update jd_price.js

--- jd_price.js
+++ jd_price.js
@@ -3,11 +3,6 @@
 http-response ^https?://api\.m\.jd\.com/client\.action\?functionId=(wareBusiness|serverConfig) script-path=jd_history_price.js,requires-body=1
 [MITM]
 hostname = api.m.jd.com
- */
-
-/*
-README：https://github.com/yichahucha/surge/tree/master
- */
 
 const path1 = "serverConfig";
 const path2 = "wareBusiness";
