734744dd06788b3acb90edca67bf8cf2706c74ce
王昭 <877820030@qq.com>
2020-01-03 10:04:20 +0800

    Update Hourly weather.js

--- Hourly weather.js
+++ Hourly weather.js
@@ -1,5 +1,5 @@
 const address = "&location=114.4956339989924,37.79361180964651";
-const k = "&key=faead2b8e432562494052aee3104bccb89d43";
+const k = "&key=2b8e432562494052aee3104bccb89d43";
 
 
 const wea = "https://free-api.heweather.net/s6/weather/now?"+address+k;
