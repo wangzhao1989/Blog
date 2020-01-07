6d8f08c73d11cb6d0a70eee188d0fb86d9732fb6
王昭 <877820030@qq.com>
2020-01-07 10:17:54 +0800

    Update YouTube.js

--- YouTube.js
+++ YouTube.js
@@ -1,3 +1,11 @@
+/*
+[Script]
+http-request ^https://[\s\S]*\.googlevideo\.com/.*&(oad|ctier) script-path=https://Choler.github.io/Surge/Script/YouTube.js
+
+[MITM]
+hostname = *.googlevideo.com
+*/
+
 var data = {
   body: "{}",
   headers: {
