17e03f1492c17b5044b9c8e750a16b5ca6179611
王昭 <877820030@qq.com>
2020-01-23 11:25:45 +0800

    Update weather_pro.js

--- weather_pro.js
+++ weather_pro.js
@@ -13,7 +13,7 @@ let config = {
     show: {
         //普通每天的
         template: {
-            title:  `$[city]$[district]$[summary]`,
+            title:  `$[city]$[district] $[summary]`,
             subtitle: `$[weatherIcon]$[weather] $[temperatureMin] ~ $[temperatureMax]°C ☔️降雨概率 $[precipProbability]%`,
             detail: `🥵空气质量 $[aqi]($[aqiDesc]) 🍃风速$[windSpeed]km/h $[windDir]
 🌞紫外线指数 $[uv]($[uvDesc]) 💧湿度$[currentHumidity]%
