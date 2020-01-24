ead72288f21b3e01b833ad6c39235bf5f4114e4d
王昭 <877820030@qq.com>
2020-01-24 08:07:03 +0800

    Update weather_pro.js

--- weather_pro.js
+++ weather_pro.js
@@ -166,10 +166,13 @@ let config = {
             title:  `$[city]$[district]  $[summary]`,
             subtitle: `$[weatherIcon]$[weather] $[temperatureMin] ~ $[temperatureMax]°C ☔️降雨概率 $[precipProbability]%`,
             detail: `🥵空气质量 $[aqi]($[aqiDesc]) 🍃风速$[windSpeed]km/h $[windDir]
-🌞紫外线指数 $[uv]($[uvDesc]) 💧湿度$[currentHumidity]%
-🌡体感温度 $[apparentTemperatureMin] ~ $[apparentTemperatureMax]°C 💨气压$[atmosphere]pa
+
 `$[hourly($[month]/$[day] $[hour]:00 $[temperature]°C  $[weatherIcon]$[weather] 湿度$[humidity]%)]`
         }
+
+🌞紫外线指数 $[uv]($[uvDesc]) 💧湿度$[currentHumidity]%
+🌡体感温度 $[apparentTemperatureMin] ~ $[apparentTemperatureMax]°C 💨气压$[atmosphere]pa
+
 [生活指数]
 $[lifeStyle($[icon][$[brf]]$[txt])]
 
