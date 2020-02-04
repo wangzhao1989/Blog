d42d44334caeea0e42dec54fb3b1679fab4d62af
王昭 <877820030@qq.com>
2020-02-04 10:25:32 +0800

    Update cpdq.js

--- cpdq.js
+++ cpdq.js
@@ -2,7 +2,8 @@
 菜谱大全 unlock vip
 
 #菜谱大全解锁vip
-https?:\/\/api\.jiaonizuocai\.com url script-response-body cpdq.js
+
+http-response ^https?:\/\/api\.jiaonizuocai\.com requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/wangzhao1989/Blog/Surge/cpdq.js
 
 
 MITM = api.jiaonizuocai.com
