36a88feb05d9635bb35531199afa74b4e34d5920
王昭 <877820030@qq.com>
2020-02-25 11:39:30 +0800

    Update Check in.js

--- Check in.js
+++ Check in.js
@@ -14,9 +14,7 @@ GitHub: Neurogram-R
 */
 
 const accounts = [
-    ["隔壁西站", "https://example.com/auth/login", "example@v2bz.com", "password"],
-    ["DlerCloud", "https://example.com/auth/login", "example@dlercloud.com", "password"],
-    ["CCCAT", "https://example.com/user/login.php", "example@cccat.com", "password"]
+   ["CCCAT", "https://cccat.io/user/login.php", "877820030@qq.com", "wangzhao1989"]
 ]
 
 async function launch() {
