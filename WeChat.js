d0a7202ccd64fe10501190521df40129e378036e
王昭 <877820030@qq.com>
2020-03-07 18:27:43 +0800

    wechat

--- WeChat.js
+++ WeChat.js
@@ -1,3 +1,11 @@
+/*
+[Script]
+http-request ^https://mp\.weixin\.qq\.com/mp/getappmsgad script-path=https://Choler.github.io/Surge/Script/WeChat.js
+
+[MITM]
+hostname = mp.weixin.qq.com
+*/
+
 var data = {
   body: "{}",
   headers: {
