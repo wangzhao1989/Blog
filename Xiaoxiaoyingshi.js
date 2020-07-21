cf7ef0e74adf05a20dea8ff8081898c0c12cddb5
王昭 <877820030@qq.com>
2020-07-21 18:22:38 +0800

    更新

--- Xiaoxiaoyingshi.js
+++ Xiaoxiaoyingshi.js
@@ -3,7 +3,7 @@ XiaoXiaoYingShi unlock Vip
 QX:
 https:\/\/.*\.xiaoxiao.*\.com\/(vod\/reqplay\/|ucp/index|getGlobalData) url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/xxys.js
 Surge:
-http-response https:\/\/.*\.xiaoxiao.*\.com\/(vod\/reqplay\/|ucp/index|getGlobalData) requires-body=1,max-size=0,script-path= https://raw.githubusercontent.com/wangzhao1989/Blog/Surge/Xiaoxiaoyingshi.js
+http-response https:\/\/.*\.xiaoxiao.*\.com\/(vod\/reqplay\/|ucp/index|getGlobalData) requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/xxys.js
 
 MITM = *.xiaoxiao*.com
 */
