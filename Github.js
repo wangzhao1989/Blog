950b30a616a960da712dc2c720412ae9210322d5
王昭 <877820030@qq.com>
2020-08-24 16:02:51 +0800

    更新

--- Github.js
+++ Github.js
@@ -17,7 +17,7 @@
  * 📌 如果希望监控Release，请切换至Release界面，直接复制URL填入；
  */
 
-let token = "784a03feb07989d3339dfa41c7eb41777436cbfa";
+let token = "da63796712122b3d9f456d5e16431c04f4ce55a2";
 
 let repositories = [
     {
