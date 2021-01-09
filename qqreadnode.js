a63fbf89ba23149c233f30f4f3ea81ad4be85be1
王昭 <877820030@qq.com>
2021-01-09 16:29:10 +0800

    更新

--- qqreadnode.js
+++ qqreadnode.js
@@ -67,7 +67,7 @@ http-request https:\/\/mqqapi\.reader\.qq\.com\/mqq\/addReadTimeWithBid? script-
 
 */
 
-const BOX = 1;//设置为0 日常任务，设置为1 单开宝箱，设置为2 完整功能版
+const BOX = 2;//设置为0 日常任务，设置为1 单开宝箱，设置为2 完整功能版
 
 
 
